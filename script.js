2615f6f47058046f0e8b50f3309fa2e00149ab16
Skylake02 <mattngaw02@gmail.com>
2018-08-04 07:59:08 -0400

    added ui logic

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 (function () {
     'use strict';
 
-    var number1 = [1, 3], number2 = [-2, 2];
+    var form            = document.querySelector('form'),
+        radios          = document.querySelectorAll('input[type=\"radio\"]'),
+        i,
+        calculation,
+        answer;
 
     function add(one, two) {
         return [one[0] + two[0], one[1] + two[1]];
@@ -20,4 +24,33 @@
         return [((one[0] * two[0]) + (one[1] * two[1])) / three,
                 ((one[0] * -two[1]) + (one[1] * two[0])) / three];
     }
+    
+    form.addEventListener('submit', function () {
+        event.preventDefault();
+        var number1     = [parseInt(document.getElementById('1r').value, 10),
+                           parseInt(document.getElementById('1i').value, 10)],
+            number2     = [parseInt(document.getElementById('2r').value, 10),
+                           parseInt(document.getElementById('2i').value, 10)];
+        for (i = 0; i < radios.length; i += 1) {
+            if (radios[i].checked) {
+                calculation = radios[i].value;
+            }
+        }
+        switch (calculation) {
+        case "add":
+            answer = add(number1, number2);
+            break;
+        case "subtract":
+            answer = subtract(number1, number2);
+            break;
+        case "multiply":
+            answer = multiply(number1, number2);
+            break;
+        case "divide":
+            answer = divide(number1, number2);
+            break;
+        }
+        document.getElementById('ar').textContent = Math.round(answer[0] * 100) / 100;
+        document.getElementById('ai').textContent = Math.round(answer[1] * 100) / 100;
+    }, false);
 }());
